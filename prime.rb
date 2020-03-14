f971523659bfc8b7e619a1b0e096d80504587305
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-14 21:19:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(value)
+  if value < 2 #|| value == 0 || Value == 1 
+    return false 
+  else 
+    (2..value - 1).none? do |i|
+      value % i == 0 
+    end 
+  end 
+end 
+  
