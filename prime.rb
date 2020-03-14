3c1d0c7e1eab204f0c5352da2438c043d3d76f7f
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-14 21:16:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,9 @@
 def prime?(value)
-  if value <= 1 || value == 0 || Value == 1 
+  if value < 2 #|| value == 0 || Value == 1 
     return false 
-  elsif 
-    (2..value - 1). each do |i|
-      if value % i == 0 
-        return false 
-      end 
+  else 
+    (2..value - 1).none? do |i|
+      value % i == 0 
     end 
   end 
   true 
